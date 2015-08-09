dc2dac1e11e7e02275099e5456d695ca5c842308
Scott Ringwelski <sgringwe@mtu.edu>
2015-08-08 22:41:46 -0700

    fix rake task loading

--- lib/handshake_service/railtie.rb
+++ lib/handshake_service/railtie.rb
@@ -4,6 +4,13 @@ module HandshakeService
   require 'rails'
 
   class Railtie < Rails::Railtie
-    rake_tasks { load "tasks/paperclip.rake" }
+    rake_tasks do
+      load "tasks/auto_annotate_models.rake"
+      load "tasks/deploy.rake"
+      load "tasks/elasticsearch.rake"
+      load "tasks/heroku.rake"
+      load "tasks/invalids.rake"
+      load "tasks/rspec_generator.rake"
+    end
   end
 end
