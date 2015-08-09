581ea7c632ccc8f14171c4129258aa6f44457bf4
Scott Ringwelski <sgringwe@mtu.edu>
2015-08-08 22:48:18 -0700

    simpler loading of rake tasks

--- lib/handshake_service/railtie.rb
+++ lib/handshake_service/railtie.rb
@@ -1,16 +1,11 @@
 require 'handshake_service'
+require 'rails'
 
 module HandshakeService
-  require 'rails'
 
   class Railtie < Rails::Railtie
     rake_tasks do
-      load "tasks/auto_annotate_models.rake"
-      load "tasks/deploy.rake"
-      load "tasks/elasticsearch.rake"
-      load "tasks/heroku.rake"
-      load "tasks/invalids.rake"
-      load "tasks/rspec_generator.rake"
+      Dir[File.expand_path("tasks/*.rake", File.dirname(__FILE__))].each { |ext| load ext }
     end
   end
 end
