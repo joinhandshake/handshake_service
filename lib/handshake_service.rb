74872ddc1e9e8e61d20a48b472102f1882006ed2
Scott Ringwelski <sgringwe@mtu.edu>
2015-08-08 23:11:13 -0700

    remove lib/ and try direct load

--- lib/handshake_service.rb
+++ lib/handshake_service.rb
@@ -1,5 +1,5 @@
 require "handshake_service/version"
-require 'handshake_service/railtie' if defined?(Rails)
+require 'handshake_service/railtie' if defined?(Rails::Railtie)
 
 module HandshakeService
   # Your code goes here...
--- lib/handshake_service/railtie.rb
+++ lib/handshake_service/railtie.rb
@@ -1,7 +1,8 @@
 module HandshakeService
   class Railtie < Rails::Railtie
     rake_tasks do
-      Dir[File.expand_path("lib/tasks/*.rake", File.dirname(__FILE__))].each { |ext|
+      load 'tasks/deploy.rake'
+      Dir[File.expand_path("tasks/*.rake", File.dirname(__FILE__))].each { |ext|
         puts ext
         puts load ext
       }
