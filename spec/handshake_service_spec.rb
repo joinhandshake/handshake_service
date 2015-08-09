8f1d722875f862b55fba025c62629c3ea1ad64b4
Scott Ringwelski <sgringwe@mtu.edu>
2015-08-08 23:43:25 -0700

    remove failing spec

--- spec/handshake_service_spec.rb
+++ spec/handshake_service_spec.rb
@@ -4,8 +4,4 @@ describe HandshakeService do
   it 'has a version number' do
     expect(HandshakeService::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
