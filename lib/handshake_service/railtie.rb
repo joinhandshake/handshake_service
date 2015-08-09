e1672a0237bfa7a709a2866efbba7649af25e5bb
Scott Ringwelski <sgringwe@mtu.edu>
2015-08-08 23:06:57 -0700

    debug

--- lib/handshake_service/railtie.rb
+++ lib/handshake_service/railtie.rb
@@ -1,7 +1,10 @@
 module HandshakeService
   class Railtie < Rails::Railtie
     rake_tasks do
-      Dir[File.expand_path("lib/tasks/*.rake", File.dirname(__FILE__))].each { |ext| load ext }
+      Dir[File.expand_path("lib/tasks/*.rake", File.dirname(__FILE__))].each { |ext|
+        puts ext
+        puts load ext
+      }
     end
   end
 end
